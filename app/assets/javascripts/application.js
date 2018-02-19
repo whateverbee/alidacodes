1768a8a32c8e8f68ad149358978226fb3029734d
Alida Weatherbee <alida.weatherbee@shopify.com>
2018-02-19 18:12:11 -0500

    fighting with sass

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -32,3 +32,13 @@ $(document).ready(function() {
     return false;
   });
 });
+
+var acc = document.getElementsByClassName('accordion');
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].onclick = function() {
+    this.classList.toggle('active');
+    this.nextElementSibling.classList.toggle('show');
+  };
+}
