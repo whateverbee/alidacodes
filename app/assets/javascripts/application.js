f20b1d630aef7aa76eb18b7816659309233d70f7
Alida Weatherbee <alida.weatherbee@shopify.com>
2018-02-19 13:56:39 -0500

    more work

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,49 @@
 $(window).load(function() {
   $('#welcomeModal').modal('show');
 });
+
+$(document).ready(function() {
+  $('#nameSubmit').click(function() {
+    e.preventDefault();
+    $(this)
+      .closest('.yep')
+      .addClass('.nope');
+    $(this)
+      .closest('.aftersubmit')
+      .removeClass('.nope');
+  });
+});
+
+function setCookie(c_name, value, expiredays) {
+  var exdate = new Date();
+  exdate.setDate(exdate.getDate() + expiredays);
+  document.cookie =
+    c_name +
+    '=' +
+    value +
+    ';path=/' +
+    (expiredays == 7 ? '' : ';expires=' + exdate.toGMTString());
+}
+function getCookie(name) {
+  var dc = document.cookie;
+  var prefix = name + '=';
+  var begin = dc.indexOf('; ' + prefix);
+  if (begin == -1) {
+    begin = dc.indexOf(prefix);
+    if (begin != 0) return null;
+  } else {
+    begin += 2;
+  }
+  var end = document.cookie.indexOf(';', begin);
+  if (end == -1) {
+    end = dc.length;
+  }
+  return unescape(dc.substring(begin + prefix.length, end));
+}
+function savename() {
+  setCookie('SetName', document.getElementById('textName').value, 1);
+}
+function getName() {
+  var strVal = getCookie('SetName');
+  var name = document.getElementById('textName').value;
+}
