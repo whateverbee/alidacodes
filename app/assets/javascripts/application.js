ba7279a78eab3f996aaa0d9e6d054db191e08aef
Alida Weatherbee <alida.weatherbee@shopify.com>
2018-02-19 18:10:26 -0500

    fighting with sass

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,51 +15,20 @@
 //= require turbolinks
 //= require_tree .
 
-$(window).load(function() {
-  $('#welcomeModal').modal('show');
-});
-
 $(document).ready(function() {
-  $('#nameSubmit').click(function() {
-    $(this)
-      .closest('.yep')
-      .addClass('.nope');
-    $(this)
-      .closest('.aftersubmit')
-      .removeClass('.nope');
+  $('.accordion-button').click(function() {
+    var accordionContent = $(this).next('.accordion-content');
+    if ($(accordionContent).is(':visible')) {
+      $(this)
+        .find('span')
+        .text('+');
+      $(accordionContent).slideUp(100);
+    } else {
+      $(this)
+        .find('span')
+        .text('-');
+      $(accordionContent).slideDown(100);
+    }
+    return false;
   });
 });
-
-function setCookie(c_name, value, expiredays) {
-  var exdate = new Date();
-  exdate.setDate(exdate.getDate() + expiredays);
-  document.cookie =
-    c_name +
-    '=' +
-    value +
-    ';path=/' +
-    (expiredays == 7 ? '' : ';expires=' + exdate.toGMTString());
-}
-function getCookie(name) {
-  var dc = document.cookie;
-  var prefix = name + '=';
-  var begin = dc.indexOf('; ' + prefix);
-  if (begin == -1) {
-    begin = dc.indexOf(prefix);
-    if (begin != 0) return null;
-  } else {
-    begin += 2;
-  }
-  var end = document.cookie.indexOf(';', begin);
-  if (end == -1) {
-    end = dc.length;
-  }
-  return unescape(dc.substring(begin + prefix.length, end));
-}
-function savename() {
-  setCookie('SetName', document.getElementById('textName').value, 1);
-}
-function getName() {
-  var strVal = getCookie('SetName');
-  var name = document.getElementById('textName').value;
-}
