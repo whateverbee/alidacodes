2b2ddcec4679b23c80778f921e8c444e2000d8f7
Alida Weatherbee <alida.weatherbee@shopify.com>
2018-02-19 18:40:07 -0500

    accordion wars

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,20 +14,3 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-
-$(document).ready(function() {
-  var acc = document.getElementsByClassName('accordion');
-  var i;
-
-  for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener('click', function() {
-      this.classList.toggle('active');
-      var panel = this.nextElementSibling;
-      if (panel.style.maxHeight) {
-        panel.style.maxHeight = null;
-      } else {
-        panel.style.maxHeight = panel.scrollHeight + 'px';
-      }
-    });
-  }
-});
